9da79de00308110f5ad01e68d1c194480f656eb2
menonsree92@gmail.com <menonsree92@gmail.com>
2014-09-16 19:22:40 +0530

    first

---
+++ ps.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+int
+main(int argc, char **argv)
+{
+pid_t pid = getpid();
+fprintf(stdout, "Path to current process: '/proc/%d/'\n", (int)pid);
+return EXIT_SUCCESS;
+getch();
+}
